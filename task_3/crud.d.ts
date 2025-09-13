eabfd5057633be58aeec00a16ef6def05232b05e
Samuel  adikah Mutesh <samsmollett50@gmail.com>
2025-09-13 19:31:18 +0300

    Delete task_3 directory

--- task_3/crud.d.ts
+++
@@ -1,7 +0,0 @@
-// task_3/js/crud.d.ts
-
-import { RowID, RowElement } from "./interface";
-
-export function insertRow(row: RowElement): RowID;
-export function deleteRow(rowId: RowID): void;
-export function updateRow(rowId: RowID, row: RowElement): RowID;
